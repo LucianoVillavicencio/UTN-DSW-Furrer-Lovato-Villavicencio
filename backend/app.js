4016653f2fc1cc43f823304c9480c28d8310cb47
Gabriel Lovato <gabilovato45@gmail.com>
2026-06-02 20:33:42 -0300

    Conexion a BD

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 
-
+import db from "./config/db.js";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -19,3 +19,15 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+
+db.query("SELECT 1")
+  .then(() => {
+    console.log("Conectado a MySQL");
+  })
+  .catch((error) => {
+    console.error("Error MySQL:", error.message);
+  });
+
+db.query("SELECT 1")
+  .then(() => console.log("Conectado a MySQL"))
+  .catch((err) => console.error("Error MySQL:", err.message));
