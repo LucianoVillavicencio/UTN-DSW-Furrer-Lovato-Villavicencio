dfcf8c29a075f5f96649787318731c05a55ebd60
Gabriel Lovato <gabilovato45@gmail.com>
2026-06-30 23:05:31 -0300

    actualizacion backend

--- back/src/modules/user/user.service.ts
+++ back/src/modules/user/user.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from "./entity/users.entity";
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
 
-    constructor(){
+    constructor(@InjectRepository(Users) private usersRepository:Repository <Users>) {
 
     }
 }
