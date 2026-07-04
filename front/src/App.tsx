2ca4e13742e0362bb7682e641b184813c71163ed
Gabrielovato <gabilovato45@gmail.com>
2026-07-04 18:54:14 -0300

    Configuración de frontend con react y ts

---
+++ front/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        FLG 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
