ee1823e5d317cce6047a37c65ff17f6be73995c2
Gabrielovato <gabilovato45@gmail.com>
2026-07-07 20:10:45 -0300

    Inicio  CRUD

--- back/src/modules/user/user.controller.ts
+++ back/src/modules/user/user.controller.ts
@@ -1,13 +1,52 @@
-import { Controller } from '@nestjs/common';
+
+import { Controller , Body , Post , Get ,Param } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
+import { UsersDto } from './dto/users-dto';
+
+
 
 @Controller('api/v1/user')
 @ApiTags('Usuarios')
 export class UserController {
 
-    constructor(private userService:UserService){
+    constructor(private userService:UserService){}
+
+    //create user
+    @Post()
+    createUsers(@Body() user: UsersDto){
+
+        return this.userService.createUsers(user);
 
     }
+
+    //get one user
+    @Get('/:id')
+    getUserById(@Param('id') idUsuario: number){
+
+        return this.userService.findUser(idUsuario);
+    }
+
+    //get all user
+    @Get()
+    getUsers(){
+
+        return this.userService.findAll();
+    }
+    
+
+    //get deleted user
+    @Get('filter/deleted')
+    getUsersDeleted(){
+        return this.userService.findAllDeleted();
+    }
+
+
+
+
+
+
+
+
 }
 
--- back/src/modules/user/user.service.ts
+++ back/src/modules/user/user.service.ts
@@ -1,12 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from "./entity/users.entity";
 import { Repository } from 'typeorm';
+import { UsersDto } from './dto/users-dto';
 
 @Injectable()
 export class UserService {
 
-    constructor(@InjectRepository(Users) private usersRepository:Repository <Users>) {
+    constructor(@InjectRepository(Users) private usersRepository:Repository <Users>) {}
 
+    async createUsers(user : UsersDto){
+
+        const userExists = await this.findUser(user.idUsuario);
+
+        if(userExists){
+
+            throw new ConflictException('El usuario con ID: ' + user.idUsuario + 'existe.')
+        } else {
+            return await this.usersRepository.save(user);
+        }
+
+    }
+
+    async findUser(idUsuario: number){
+
+        return await this.usersRepository.findOne({ where: {idUsuario}});
+
+    }
+
+    async findAll(){
+
+        return await this.usersRepository.find({where : {deleted : false}});
+
+    }
+
+
+    // Users deleted
+    async findAllDeleted(){ 
+
+        return await this.usersRepository.find({where : {deleted : true}});
+        
     }
 }
