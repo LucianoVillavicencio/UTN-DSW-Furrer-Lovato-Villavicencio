ef217bf5241f24832be0e5438e3a0d11853839b7
Gabrielovato <gabilovato45@gmail.com>
2026-07-13 15:52:54 -0300

    feat: integro ProgramsSection en Home

--- front/src/pages/Home/Home.tsx
+++ front/src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Navbar from "../../components/layout/Navbar";
-import Hero from "../../components/layout/HeroHome";
+import Hero from "../../components/home/HeroSection";
+import ProgramSection from "../../components/home/ProgramsSection";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <ProgramSection />
     </>
   );
 }
