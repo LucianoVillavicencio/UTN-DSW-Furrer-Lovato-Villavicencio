14d5954314e1baed5e9d813e283c7f50931fb2e8
Gabrielovato <gabilovato45@gmail.com>
2026-07-13 15:52:12 -0300

    feat: agrego ProgramsSection con ProgramCard

---
+++ front/src/components/home/ProgramCard.tsx
@@ -0,0 +1,30 @@
+import type { LucideIcon } from "lucide-react";
+import Card from "../common/Card";
+
+interface ProgramCardProps {
+    icon: LucideIcon;
+    title: string,
+    descripcion: string
+    
+}
+
+
+const ProgramCard = ({icon: Icon, title, descripcion }:ProgramCardProps) => {
+  return (
+
+    <Card className="text-center">
+        <div className="mx-auto flex h-16 w-16 items-center justify-center rounded-full transition-all duration-200 group-hover:bg-green-800/20 ">
+            <Icon className="h-8 w-8 text-primary"></Icon>
+        </div>
+        <div>
+            <title></title>
+        </div>
+        
+        <h3 className="mt-5 text-xl font-semibold text-text">{title}</h3>
+
+        <p className=" mt-2 font-body text-sm text-text-muted ">{descripcion}</p>
+    </Card>
+  );
+}
+
+export default ProgramCard;
