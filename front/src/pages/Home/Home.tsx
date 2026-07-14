b859af4a6c45928cfbc94109de7616bdad085753
Gabrielovato <gabilovato45@gmail.com>
2026-07-14 18:09:13 -0300

    feat: integro CTAsection en home

--- front/src/pages/Home/Home.tsx
+++ front/src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import Navbar from "../../components/layout/Navbar";
 import Hero from "../../components/home/HeroSection";
 import ProgramSection from "../../components/home/ProgramsSection";
 import TestimonialSection from "../../components/home/TestimonialSection";
+import CTASection from "../../components/home/CTASection";
 
 function Home() {
   return (
@@ -10,6 +11,8 @@ function Home() {
       <Hero />
       <ProgramSection />
       <TestimonialSection />
+      <CTASection />
+
     </>
   );
 }
