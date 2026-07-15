c8a14ca016e7bd65f5b4be6dfb6d5cfd29a289a4
Gabrielovato <gabilovato45@gmail.com>
2026-07-14 23:15:16 -0300

    fix: elimino etiqueta de title y agrego efecto hover en classCard

--- front/src/components/home/ClassCard.tsx
+++ front/src/components/home/ClassCard.tsx
@@ -9,22 +9,18 @@ interface ClassCardProps {
 }
 
 
-const ClassCard = ({icon: Icon, title, descripcion }:ClassCardProps) => {
+const ClassCard = ({ icon: Icon, title, descripcion }: ClassCardProps) => {
   return (
-
     <Card className="text-center">
-        <div className="mx-auto flex h-16 w-16 items-center justify-center rounded-full transition-all duration-200 group-hover:bg-green-800/20 ">
-            <Icon className="h-8 w-8 text-primary"></Icon>
-        </div>
-        <div>
-            <title></title>
-        </div>
-        
-        <h3 className="mt-5 text-xl font-semibold text-text">{title}</h3>
+      <div className="mx-auto flex h-16 w-16 items-center justify-center rounded-full transition-all duration-200 group-hover:bg-green-800/20">
+        <Icon className="h-8 w-8 text-primary" />
+      </div>
 
-        <p className=" mt-2 font-body text-sm text-text-muted ">{descripcion}</p>
+      <h3 className="mt-5 text-xl font-semibold text-text">{title}</h3>
+
+      <p className="mt-2 font-body text-sm text-text-muted">{descripcion}</p>
     </Card>
   );
-}
+};
 
 export default ClassCard;
