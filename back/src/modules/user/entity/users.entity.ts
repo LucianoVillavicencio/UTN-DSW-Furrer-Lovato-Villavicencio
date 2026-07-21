427f672a9d81ed1a883ea0268129df37b846bb71
Gabriel Lovato <gabilovato45@gmail.com>
2026-07-21 20:15:02 -0300

    feat: cambie idUsuario por dni

--- back/src/modules/user/entity/users.entity.ts
+++ back/src/modules/user/entity/users.entity.ts
@@ -1,28 +1,25 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('users')
+export class Users {
+  @PrimaryGeneratedColumn()
+  dni!: number;
 
-@Entity("users")
+  @Column({ type: String, nullable: false, length: 30 })
+  email!: string;
 
-export class Users{
-    
-    @PrimaryGeneratedColumn()
-    idUsuario!: number;
+  @Column({ type: String, nullable: false, length: 20 })
+  name!: string;
 
-    @Column({type: String, nullable:false, length: 30})
-    email!: string;
+  @Column({ type: String, nullable: false, length: 20 })
+  surname!: string;
 
-    @Column({type: String, nullable:false, length: 20})
-    name!: string;
+  @Column({ type: String, nullable: false, length: 20 })
+  phone!: string;
 
-    @Column({type: String, nullable:false, length: 20})
-    surname!: string;
+  @Column({ type: String, nullable: false, length: 30 })
+  password!: string;
 
-    @Column({type: String, nullable:false, length: 20})
-    phone!: string;
-
-    @Column({type: String, nullable:false, length: 30})
-    password!: string;
-
-    @Column({type: Boolean, nullable:false, default:false})
-    deleted!: boolean;
+  @Column({ type: Boolean, nullable: false, default: false })
+  deleted!: boolean;
 }
