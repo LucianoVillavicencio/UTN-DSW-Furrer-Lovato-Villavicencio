2452b215b311c902bb0fd96237dfefd4ba3b4630
Gabriel Lovato <gabilovato45@gmail.com>
2026-08-05 19:56:32 -0300

    refactor: actualizo ruta de classSection en Home

--- front/src/pages/Home/Home.tsx
+++ front/src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import Navbar from "../../components/layout/Navbar";
 import HeroSection from "../../components/home/HeroSection";
-import ClassesSection from "../../components/common/ClassesSection";
+import ClassesSection from "../../components/home/ClassesSection";
 import TestimonialsSection from "../../components/home/TestimonialsSection";
 import CTASection from "../../components/common/CTASection";
 import Footer from "../../components/layout/Footer";
