a356b0bae1c66c4ceca5d245c0b088388abee002
Gabriel Lovato <gabilovato45@gmail.com>
2026-08-11 23:51:41 -0300

    refactor: reemplazar ClassesHeroSection por el PageHeader compartido

--- front/src/pages/Classes/Classes.tsx
+++ front/src/pages/Classes/Classes.tsx
@@ -1,13 +1,24 @@
 import Navbar from "../../components/layout/Navbar";
 import Footer from "../../components/layout/Footer";
-import ClassesHeroSection from "../../components/classes/ClassesHeroSection";
+import PageHeader from "../../components/common/PageHeader";
 import ClassEnrollmentSection from "../../components/classes/ClassEnrollmentSection";
+import { LayoutGridIcon } from "lucide-react";
+
 
 function Classes() {
   return (
     <>
       <Navbar />
-      <ClassesHeroSection />
+      <PageHeader
+        badge="Nuestras clases"
+        icon={LayoutGridIcon}
+        title={
+          <>
+            Encuentra tu clase <span className="text-primary">perfecta</span>
+          </>
+        }
+        subtitle="Contamos con una variedad de clases diseñadas para todos los niveles de experiencia. Desde principiantes hasta atletas avanzados, tenemos el entrenamiento perfecto para ti."
+      />
       <ClassEnrollmentSection />
       <Footer />
     </>
